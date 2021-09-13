4fa072b0f9ce8eba07cfa48403501cdca1c340fe
Colin Wilson <colin.chris.wilson@gmail.com>
2021-09-12 20:30:02 -0400

    update readme

--- LearnerProgram2/00runme.sh
+++
@@ -1,2 +0,0 @@
-# Run English past tense simulation
-java -server -cp $CLASSPATH:extern/snakeyaml-1.29.jar:bin/mingenlearn.jar LearnerCommandline EnglishPastTense.yaml
---
+++ version2/00runme.sh
@@ -0,0 +1,2 @@
+# Run English past tense simulation
+java -server -cp $CLASSPATH:extern/snakeyaml-1.29.jar:bin/mingenlearn.jar LearnerCommandline english.yaml
