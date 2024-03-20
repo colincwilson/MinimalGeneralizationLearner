9b858d1d6129c2687b27967c3de6d1bc2a57502a
Colin Wilson <colin@cogsci.jhu.edu>
2024-03-19 22:57:55 -0400

    fix english ipa data

--- english/English2_ipa/unimorph_format.R
+++ english/English2_ipa/unimorph_format.R
@@ -1,20 +1,19 @@
 require(tidyverse)
 
-setwd('~/Researchers/HayesBruce/AlbrightHayes2003/English2_ipa')
+setwd("~/Code/MinimalGeneralizationLearner/english/English2_ipa")
 
-for (fdat_in in c('CELEXFull.in', 'CELEXPrefixStrip.in')) {
+for (fdat_in in c("CELEXFull_ipa.in", "CELEXPrefixStrip_ipa.in")) {
+    dat_in <- read_tsv(fdat_in)
 
-dat_in = read_tsv(fdat_in)
-
-dat_in %>%
-    mutate(lemma_ipa = gsub("ˈ", '', lemma_ipa)) %>%
-    mutate(past_ipa = gsub("ˈ", '', past_ipa)) %>%
-    mutate(morphosyn='V;PST;') %>%
-    select(lemma_ipa, past_ipa, morphosyn, lemma_orth, past_orth) %>%
-    #drop_na() %>%
-    identity() -> 
+    dat_in %>%
+        mutate(lemma_ipa = gsub("ˈ", "", lemma_ipa)) %>%
+        mutate(past_ipa = gsub("ˈ", "", past_ipa)) %>%
+        mutate(morphosyn = "V;PST;") %>%
+        select(lemma_ipa, past_ipa, morphosyn, lemma_orth, past_orth) %>%
+        # drop_na() %>%
+        identity() ->
     dat_out
 
-fdat_out = str_glue('{fdat_in}.unimorph')
-#write_tsv(dat_out, fdat_out, col_names=FALSE)
+    fdat_out <- str_glue("{fdat_in}.unimorph")
+    # write_tsv(dat_out, fdat_out, col_names=FALSE)
 }
